a83a51ca738d6c63edad4da1c05e0711f4ee4a52
toshi1048 <toshiya.22.1048@gmail.com>
2020-09-30 11:52:23 +0900

    userモデル作成、管理者権限ユーザーadmin追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_080943) do
+ActiveRecord::Schema.define(version: 2020_09_30_020959) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
